d2d7b8acd0f2658378bcc576b7a763f8ec51b87a
ariez <dpape39@icloud.com>
2026-04-01 13:44:47 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sablas",
+    version="0.0.0",
+    description="Safe Control for Black-box Dynamical Systems via Neural Barrier Certificates",
+    author="Zengyi Qin",
+    url="https://github.com/ariez-xyz/sablas/",
+    install_requires=[],
+    packages=find_packages(),
+)
